60bfd296b9fdbb427ccff03dff2d5105e867f94b
Frank Kevin Zey <frank.kevin.zey@outlook.com>
2024-10-21 22:44:50 +0200

    adjusts Scope namespace

--- Include/mstl/scope.hpp
+++ Include/mstl/scope.hpp
@@ -2,11 +2,11 @@
 
 #include <memory>
 
-template<typename T>
-using Scope = std::unique_ptr<T>;
-
-namespace Moebius
+namespace mstl
 {
+
+	template<typename T>
+	using Scope = std::unique_ptr<T>;
 
 	template<typename T, typename ... Args>
 	constexpr Scope<T> CreateScope(Args&& ... args) { return std::make_unique<T>(std::forward<Args>(args)...); }
