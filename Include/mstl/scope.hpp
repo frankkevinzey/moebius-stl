c3dda14decc595d5cb1de37968ba71047d1968a0
Frank Kevin Zey <frank.kevin.zey@outlook.com>
2026-07-18 15:41:41 +0200

    Moves Scope<T> more near unique_pointer behaviour

--- Include/mstl/scope.hpp
+++ Include/mstl/scope.hpp
@@ -5,15 +5,17 @@
 namespace mstl
 {
 
+	/// <summary>
+	/// Scopes an objects ownership. Ownership is not transferable by assignment but explicit function calls.
+	/// </summary>
+	/// <typeparam name="T">Type of object to scope ownership</typeparam>
 	template<typename T>
 	class Scope final
 	{
 	public:
-
-	public:
 		Scope(void) = default;
 		inline Scope(std::nullptr_t n) : _ptr(nullptr) {}
-		inline Scope(T* instance) : _ptr(instance) {}
+		inline Scope(T instance) { _ptr = new T(std::move(instance)); }
 		inline ~Scope(void) { Release(); }
 
 		Scope& operator=(const Scope<T>& other) = delete;
@@ -35,20 +37,17 @@ namespace mstl
 			return *this;
 		}
 
-		template<typename T>
-		inline T& operator *(void) { return *_ptr; }
+		template<typename T2>
+		inline T2* As(void) { return (T2*)_ptr; }
 
-		inline operator bool(void) { return _ptr != nullptr; }
+		inline operator bool(void)       { return _ptr != nullptr; }
 		inline operator bool(void) const { return _ptr != nullptr; }
 
 		inline T* operator->(void) { return _ptr; }
 		inline const T* operator->(void) const { return _ptr; }
 
-		inline T& operator*(void) { return *_ptr; }
-		inline const T& operator*(void) const { return *_ptr; }
-
-		inline T* Raw(void) { return _ptr; }
-		inline const T* Raw(void) const { return _ptr; }
+		inline T* const Raw(void) { return _ptr; }
+		inline const T* const Raw(void) const { return _ptr; }
 
 		inline void Reset(T* instance = nullptr)
 		{
@@ -67,6 +66,8 @@ namespace mstl
 
 		template<typename... Args>
 		static inline Scope<T> Create(Args&&... args) { return Scope<T>(new T(std::forward<Args>(args)...)); }
+
+		inline T* Transfer(void) { T* _tmp = _ptr; _ptr = nullptr; return _tmp; }
 
 	private:
 		inline void Release(void) { if (_ptr) delete _ptr; _ptr = nullptr; }
